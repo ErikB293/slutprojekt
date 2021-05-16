01e16c67564e4a1470670a59515a404b7bd84cff
Erik Björkbäck <erikbj2002@gmail.com>
2021-05-16 13:53:26 +0200

    Färdigt arbete

--- JAVA/accordian.js
+++ JAVA/accordian.js
@@ -5,11 +5,11 @@ function openPanel(y) {
     let x = document.getElementById("avAcc" + y.id);
 
     
-        if (x.style.height == "37vh") {
+        if (x.style.height == "39vh") {
             x.style.height = "21.5vh";
             console.log("den är ihopdragen");
         } else {
-            x.style.height = "37vh";
+            x.style.height = "39vh";
             console.log("Ut dragen");
         }
     
